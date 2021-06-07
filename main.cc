023f87053e2417405e36a914f9021fb230e2ee18
unyuu <u@nyuu.ru>
2021-06-07 20:29:46 +0500

    Часть того, что должно бы быть глобальным, сделана глобальной.

--- main.cc
+++ main.cc
@@ -226,6 +226,19 @@ void writeGenRes(std::vector<std::shared_ptr<Enemy>> sortEnemy,
 	out.close();
 }
 
+std::random_device random_device;
+std::mt19937 engine{ random_device() };
+std::uniform_int_distribution<> dist2(0, 9);
+std::uniform_int_distribution<> forX(1, 620);
+std::uniform_int_distribution<> forY(20, 80);
+
+std::uniform_int_distribution<> forOrder(0, 254);
+std::uniform_int_distribution<> forSplice(0, 7);
+
+constexpr double Pmut = 0.02;
+std::uniform_real_distribution<double> mut(0.0, 1.0);
+
+
 
 int main( int argc, char* args[] )
 {
@@ -240,11 +253,6 @@ int main( int argc, char* args[] )
 	SDL_Color textColor = { 0, 0, 0, 255 };
 
 	SDL_Event e;
-
-//			VisionEnemySensor *visionEnemySensor = new VisionEnemySensor;
-//			VisionDotBulletSensor *visionDotBulletSensor = new VisionDotBulletSensor;
-//			VisionAllySensor *visionAllySensor = new VisionAllySensor;
-//			VisionAllyBulletSensor *visionAllyBulletSensor = new VisionAllyBulletSensor;
 
 	VisionEnemySensorLeft *visionEnemySensorLeft = new VisionEnemySensorLeft;
 	VisionEnemySensorRight *visionEnemySensorRight = new VisionEnemySensorRight;
@@ -269,17 +277,6 @@ int main( int argc, char* args[] )
 
 	std::vector<int> sec1(sec1Length);
 	std::vector<int> sec2(sec2Length);
-	std::random_device random_device;
-	std::mt19937 engine{ random_device() };
-	std::uniform_int_distribution<> dist2(0, 9);
-	std::uniform_int_distribution<> forX(1, 620);
-	std::uniform_int_distribution<> forY(20, 80);
-
-	std::uniform_int_distribution<> forOrder(0, 254);
-	std::uniform_int_distribution<> forSplice(0, 7);
-
-	constexpr double Pmut = 0.02;
-	std::uniform_real_distribution<double> mut(0.0, 1.0);
 
 	for (int i = 0; i < NUMBEROFOPPONENTS; ++i) {
 		for (int k = 0; k < sec1Length; ++k)
